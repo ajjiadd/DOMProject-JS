3d4f321b0258d3c94d371f0fee18d353b79c979e
ajjiad <ajjiad.cse@gmail.com>
2024-01-14 18:33:51 +0600

    create genarate RGB color and showed in backGround || handels with onclick with button

---
+++ main.js
@@ -0,0 +1,25 @@
+
+window.onload = () => {
+    // main();
+};
+
+
+function main(){
+    const root = document.getElementById("root");
+    const chngBG = document.getElementById("chngBG");
+
+    chngBG.addEventListener("click", function(){
+        // console.log("click the button");
+        const color = genarateRGBcolor();
+        root.style.backgroundColor = color;
+    });
+
+};
+
+
+function genarateRGBcolor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random()* 256);
+    return "rgb(" + r + ","+g+","+b+")";
+};
